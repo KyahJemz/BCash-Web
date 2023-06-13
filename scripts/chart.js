20e0eb637c69a49f02530c265ff325201277b1d3
Stephen Regan James Bacolod Layson <jameslayson.0@gmail.com>
2023-06-14 04:48:33 +0800

    update charts

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,5 +1,8 @@
 
-  const data = {
+// ###################
+// Daily Sales
+// ###################
+  const chartDaily_Data = {
     labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
     datasets: [
       {
@@ -14,14 +17,14 @@
     ]
   };
 
-  const config = {
+  const chartDaily_Config = {
     type: 'line',
-    data: data,
+    data: chartDaily_Data,
     options: {
       responsive: true,
       plugins: {
         title: {
-          display: true,
+          display: false,
           text: (ctx) => 'Sales'
         },
         legend: {
@@ -30,6 +33,91 @@
       }
     }
   };
+
+  const chartDaily = document.getElementById('graph-daily').getContext('2d');
+  new Chart(chartDaily, chartDaily_Config);
+
+// ###################
+// Sales Per Day
+// ###################
+
+  const chatOrderByItem_Data = {
+    labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+    datasets: [
+      {
+        data: [10, 20, 15, 25, 18, 30],
+        borderColor: 'black',
+        backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(255, 206, 86, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)'],
+        borderWidth: 0.5,
+      }
+    ]
+  };
+
+  const chatOrderByItem_Config = {
+    type: 'bar',
+    data: chatOrderByItem_Data,
+    options: {
+      indexAxis: 'y',
+      responsive: true,
+      plugins: {
+        title: {
+          display: false,
+          text: (ctx) => 'Sales'
+        },
+        legend: {
+          display: false,
+          }
+      },
+      elements: {
+        bar: {
+          borderWidth: 1,
+        }
+      }
+    }
+  };
+
+  const chatOrderByItem = document.getElementById('graph-order-by-items').getContext('2d');
+  new Chart(chatOrderByItem, chatOrderByItem_Config);
+
+// ###################
+// Sales Per Day
+// ###################
+
+  const chartSalesByCategory_Data = {
+    labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+    datasets: [
+      {
+        data: [10, 20, 15, 25, 18, 30],
+        backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(255, 206, 86, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)'],
+        borderColor: 'black',
+        borderWidth: 0.5,
+      }
+    ]
+  };
+
+  const chartSalesByCategory_Config = {
+    type: 'pie',
+    data: chartSalesByCategory_Data,
+    options: {
+      responsive: true,
+      plugins: {
+        title: {
+          display: false,
+          text: (ctx) => 'Sales'
+        },
+        legend: {
+            display: true,
+          }
+      }
+    }
+  };
+
+  const chartSalesByCategory = document.getElementById('graph-sales-by-category').getContext('2d');
+  new Chart(chartSalesByCategory, chartSalesByCategory_Config);
+
+// ###################
+// Chart Events
+// ###################
 
   const actions = [
     {
@@ -42,6 +130,3 @@
       }
     }
   ];
-
-  const ctx = document.getElementById('graph-daily').getContext('2d');
-  new Chart(ctx, config);
