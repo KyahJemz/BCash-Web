df702d52ffd95c8fee39cc4373ae54c695e8c5a3
Stephen Regan James Bacolod Layson <jameslayson.0@gmail.com>
2023-06-23 03:51:45 +0800

    last morning update

--- scripts/dialog-box.js
+++ scripts/dialog-box.js
@@ -1,11 +1,17 @@
 function openDialogBox(type){
+    const box = document.getElementById("Dialog-Box-Container");
+    const header = document.getElementById("Dialog-Box-header");
+    const body = document.getElementById("Dialog-Box-Body");
     if (type=="Add-Item"){
-        const box = document.getElementById("Dialog-Box-Container");
-        const header = document.getElementById("Dialog-Box-header");
-        const body = document.getElementById("Dialog-Box-Body");
-
         box.style.display = "flex";
-    } 
+        header.innerHTML = "Add Item Form"
+    } else if (type=="Edit-Item"){
+        box.style.display = "flex";
+        header.innerHTML = "Edit Item Form"
+    } else if (type=="Delete-Item"){
+        box.style.display = "flex";
+        header.innerHTML = "Delete Item Confirmation"
+    }
 }
 
 function closeDialogBox() {
